4c2669a650aea9ffe7ac760f667eb24549b1fbc9
ritugoyal78 <73299861+ritugoyal78@users.noreply.github.com>
2021-10-19 23:50:22 +0530

    Add files via upload

--- screens/Transaction.js
+++ screens/Transaction.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {Text,View} from 'react-native';
 import * as Permissions from 'expo-permissions';
 import {BarCodeScanner} from 'expo-barcode-scanner';
-export default class Transaction extends React.Component{
+export default class Transaction extends Component{
 render(){
     return(
         <View style={{flex:1,justifyContent:'right',alignItems:'center'}}>
